02684fae19d28ab9a2e770397bdb154b812716d7
Yudha Yunanto Putra <61044350+yudhayunantop@users.noreply.github.com>
2020-12-08 12:39:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/courotineroom/NoteViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.courotineroom
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.courotineroom.catatan.Note
+import com.example.courotineroom.catatan.NoteRepository
+
+class NoteViewModel(application: Application) : AndroidViewModel(application)
+{
+    private var repository: NoteRepository =
+        NoteRepository(application)
+    private var allNotes: LiveData<List<Note>> = repository.getAllNotes()
+    fun insert(note: Note) {
+        repository.insert(note)
+    }
+    fun update(note: Note) {
+        repository.update(note)
+    }
+    fun delete(note: Note) {
+        repository.delete(note)
+    }
+    fun deleteAllNotes() {
+        repository.deleteAllNotes()
+    }
+    fun getAllNotes(): LiveData<List<Note>> {
+        return allNotes
+    }
+}
